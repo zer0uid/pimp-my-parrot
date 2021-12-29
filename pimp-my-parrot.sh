b6ad51e4584e793f589dc610204524fa0c68c8d9
zer0uid <70248159+zer0uid@users.noreply.github.com>
2021-12-29 09:57:35 -0500

    Updated comments
    Added additional URL's to import to bookmarks.  Need to look into automating this via the script.

--- pimp-my-parrot.sh
+++ pimp-my-parrot.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 # Update OS
 echo "[*] Updating OS "
@@ -9,8 +10,8 @@ sudo apt purge neovim -y
 sudo apt purge mate-terminal
 echo "[*] Installing vim"
 sudo apt install vim -y
-echo "[*] Remove default folders, Music, Videos, and Pictures"
-rm -rf Music/ Videos/ Pictures/
+echo "[*] Remove default folders, Music, Videos"
+rm -rf Music/ Videos/ 
 echo "[*] Creating TryHackMe (THM) and HackTheBox (HTB) Folders"
 mkdir THM/
 mkdir HTB/
@@ -20,30 +21,29 @@ sudo apt install spice-vdagent
 
 ## Install new apps
 echo "[*] Installing new applications"
-echo "[*] Installing requirements for Autorecon"
-sudo apt install seclists curl enum4linux feroxbuster impacket-scripts nbtscan nikto nmap onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf -y
-echo "[*] Installing Autorecon"
-sudo python3 -m pip install git+https://github.com/Tib3rius/AutoRecon.git
-echo "[*] Installing Searchsploit"
+sudo apt install ftp
+sudo apt install default-mysql-client
+echo "[*] MySQL Installed"
 sudo git clone https://github.com/offensive-security/exploitdb.git /opt/exploitdb
 sudo ln -sf /opt/exploitdb/searchsploit /usr/local/bin/searchsploit
-echo "[*] Installing TLDR"
+echo "[*] Searchsploit installed"
 sudo apt install tldr -y
 tldr -u
-echo "[*] Installing Feroxbuster"
+echo "[*] TLDR installed"
 sudo apt install feroxbuster -y
-echo "[*] Installing Ciphey"
+echo "[*] ferroxbuster installed"
 python3 -m pip install ciphey --upgrade
-echo "[*] Installing Search-That-Hash"
+echo "[*] Ciphey installed"
 pip3 install search-that-hash && sth
-echo "[*] Installing Rustscan"
+echo "[*] Search-That-Hash installed"
 wget https://github.com/RustScan/RustScan/releases/download/2.0.1/rustscan_2.0.1_amd64.deb -O /home/user/Downloads/rustscan_2.0.1_amd64.deb
 sudo dpkg -i $HOME/Downloads/rustscan_2.0.1_amd64.deb
-echo "[*] Installing ffuf"
+echo "[*] Rustscan installed"
 sudo apt install ffuf -y
-echo "[*] Installing Stegseek"
+echo "[*] ffuf installed"
 wget https://github.com/RickdeJager/stegseek/releases
 sudo dpkg -i $HOME/Downloads/stegseek_0.6-1.deb
+echo "[*] Stegseek installed"
 # Remove downloaded deb packages
 rm $HOME/Downloads/*.deb
 # Insatll Mono
@@ -57,4 +57,23 @@ sudo chmod +x NetworkMiner.exe
 sudo chmod -R go+w AssembledFiles/
 sudo chmod -R go+w Captures/ 
 
+# Install Subfinder (requires GO; which is installed on Parrot)
+# --> https://github.com/projectdiscovery/subfinder
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+
 echo "[*] Pimp My Parrot complete"
+
+## Setup LinPeas, etc, in the "Public/" folder ##
+## INSTALL .DOT FILES FROM GITHUB GISTS ##
+## Install FF Plugins: DotGit, FoxyProxy, CookieManager
+## Install Terminator Plugins: https://github.com/EliverLara/terminator-themes
+
+### Browser Bookmarks ###
+# https://gtfobins.github.io/
+# https://lolbas-project.github.io/#
+# https://book.hacktricks.xyz/
+# https://wadcoms.github.io/
+# https://github.com/RoqueNight/Reverse-Shell-TTY-Cheat-Sheet
+# https://www.dcode.fr/en
+# https://gchq.github.io/CyberChef/
+# https://www.revshells.com/
